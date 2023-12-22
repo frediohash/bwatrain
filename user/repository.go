fc7387a09ac325e00fca4d786937c174b1ceb747
fredio.dimyati <fredio.dimyati@dwidasa.com>
2023-12-22 07:20:14 +0700

    Find n Update Repository

--- user/repository.go
+++ user/repository.go
@@ -31,3 +31,20 @@ func (r *repository) FindByEmail(email string) (User, error) {
 	}
 	return user, nil
 }
+
+func (r *repository) FindByID(ID int) (User, error) {
+	var user User
+	err := r.db.Where("id = ?", ID).Find(&user).Error
+	if err != nil {
+		return user, err
+	}
+	return user, nil
+}
+
+func (r *repository) Update(user User) (User, error) {
+	err := r.db.Save(&user).Error
+	if err != nil {
+		return user, err
+	}
+	return user, nil
+}
